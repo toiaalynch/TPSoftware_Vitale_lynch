0bad1cfab09fe824fbda14ad185de4b7e934ad1f
toiaalynch <vlynch@udesa.edu.ar>
2025-09-20 13:02:09 -0300

    Primer commit

---
+++ src/main/java/org/udesa/giftcard/GiftCardApplication.java
@@ -0,0 +1,13 @@
+package org.udesa.giftcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/udesa/giftcard/GiftCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesa.giftcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
